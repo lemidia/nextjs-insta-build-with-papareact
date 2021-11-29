78ffb0ec9d21cd213c226bb58946d27c59bccbd9
lemidia <poiu2186@gmail.com>
2021-11-29 23:55:45 +0900

    edit gitignore

--- firebase.js
+++ firebase.js
@@ -2,16 +2,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-};
+import firebaseConfig from "./firebase.config";
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
